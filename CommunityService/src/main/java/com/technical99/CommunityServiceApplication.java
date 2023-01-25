ae055b783e68c35593a4ead84fd3b98bdcb6fa44
Tarique Anwer <71516598+tariquea99@users.noreply.github.com>
2023-01-25 08:54:16 +0530

    Update CommunityServiceApplication.java
    comment added to main file

--- CommunityService/src/main/java/com/technical99/CommunityServiceApplication.java
+++ CommunityService/src/main/java/com/technical99/CommunityServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunityServiceApplication {
-
+	// Main class ---
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityServiceApplication.class, args);
 	}
